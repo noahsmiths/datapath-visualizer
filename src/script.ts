974c6cc4721766c246fd0ba31ef44666d504387e
noahsmiths <90880131+noahsmiths@users.noreply.github.com>
2024-01-21 03:31:20 -0500

    Add pipeline diagram and make it toggleable

--- src/script.ts
+++ src/script.ts
@@ -3,11 +3,26 @@ import { parseInstructions, Instruction } from "./util/InstructionParser";
 const instructionInput = document.getElementById("instructions") as HTMLTextAreaElement;
 const loadInstructionsButton = document.getElementById("load-instructions-btn");
 const runSingleClockCycleButton = document.getElementById("run-single-clock-cycle-btn");
+const toggleViewButton = document.getElementById("toggle-view-btn");
+const diagramBody = document.getElementById("diagram-body") as HTMLElement;
+const diagramCountHeader = document.getElementById("diagram-count") as HTMLElement;
+const diagramHeaderDefault = document.getElementById("diagram-header-default-template") as HTMLTemplateElement;
+const pipelineContainer = document.getElementById("pipeline-container");
+const diagramContainer = document.getElementById("diagram-container");
 
 loadInstructionsButton?.addEventListener("click", loadInstructions);
 runSingleClockCycleButton?.addEventListener("click", () => {
     runSingleClockCycle();
     updateDisplay();
+});
+toggleViewButton?.addEventListener("click", () => {
+    if (pipelineContainer?.classList.contains("hidden")) {
+        pipelineContainer?.classList.remove("hidden");
+        diagramContainer?.classList.add("hidden");
+    } else {
+        pipelineContainer?.classList.add("hidden");
+        diagramContainer?.classList.remove("hidden");
+    }
 });
 
 interface StageState {
@@ -15,18 +30,57 @@ interface StageState {
     color: string | null
 }
 
-const hazardCount: Array<number> = new Array(32).fill(0);
+let hazardCount: Array<number> = new Array(32).fill(0);
 let instructionList: Array<Instruction> = [];
-const dataPathState: Array<StageState | null> = new Array(5).fill(null);
+let dataPathState: Array<StageState | null> = new Array(5).fill(null);
+const stageLabelMap = ["F", "D", "X", "M", "W"];
+
+let instructionsProcessed = 0;
+let cycleCount = 0;
 
 function loadInstructions() {
     try {
         let splitInputList = instructionInput.value.split("\n").map(el => el.trim()).filter(el => el !== "");
         let parsedInstructionList = parseInstructions(splitInputList);
         instructionList = parsedInstructionList;
+        hazardCount = new Array(32).fill(0);
+        dataPathState = new Array(5).fill(null);
+        instructionsProcessed = 0;
+        cycleCount = 0;
+
+        initializePipeline();
+        initializeDiagram();
     } catch (err) {
         alert(err);
         console.error(err);
+    }
+}
+
+function initializePipeline() {
+    for (let i = 0; i < dataPathState.length; i++) {
+        (document.getElementById(`${i}-text`) as HTMLElement).innerText = "";
+        (document.getElementById(`${i}-text`) as HTMLElement).style.color = "unset";
+        (document.getElementById(`${i}-highlight`) as HTMLElement).style.backgroundColor = "unset";
+    }
+}
+
+function initializeDiagram() {
+    diagramCountHeader.innerHTML = "";
+    for (let child of diagramHeaderDefault.content.children) {
+        diagramCountHeader.appendChild(child.cloneNode(true));
+    }
+
+    diagramBody.innerHTML = "";
+
+    for (let i = 0; i < instructionList.length; i++) {
+        let parent = document.createElement("tr");
+        // parent.id = `${i}-row`;
+
+        let instructionText = document.createElement("th");
+        instructionText.innerText = instructionList[i].raw_instruction;
+
+        parent.appendChild(instructionText);
+        diagramBody?.appendChild(parent);
     }
 }
 
@@ -38,6 +92,10 @@ const NOP: Instruction = {
 
 function runSingleClockCycle() {
     let registerToFree = dataPathState[4]?.instruction.outputRegister;
+
+    if (dataPathState[4] !== null && dataPathState[4].instruction !== NOP) {
+        instructionsProcessed++;
+    }
 
     // Memory -> Writeback
     dataPathState[4] = dataPathState[3];
@@ -71,6 +129,11 @@ function runSingleClockCycle() {
     if (Number.isInteger(registerToFree)) {
         hazardCount[registerToFree as number] -= 1;
     }
+
+    if (dataPathState.filter(el => el !== null).length > 0) {
+        cycleCount++;
+        console.log(cycleCount);
+    }
 }
 
 let availableColors = ["red", "blue", "green", "orange", "purple"];
@@ -81,6 +144,7 @@ function getColor() {
 }
 
 function updateDisplay() {
+    // Update colors on datapath
     for (let i = 0; i < dataPathState.length; i++) {
         const state = dataPathState[i];
         const color = state?.color || "unset";
@@ -89,5 +153,31 @@ function updateDisplay() {
         (document.getElementById(`${i}-text`) as HTMLElement).innerText = text;
         (document.getElementById(`${i}-text`) as HTMLElement).style.color = color;
         (document.getElementById(`${i}-highlight`) as HTMLElement).style.backgroundColor = color;
+    }
+
+    if (diagramCountHeader.children.length - 2 < cycleCount) {
+        let nextCount = document.createElement("th");
+        nextCount.className = "w-12";
+        nextCount.innerText = cycleCount.toString();
+
+        console.log(diagramCountHeader.lastChild);
+
+        diagramCountHeader.insertBefore(nextCount, diagramCountHeader.children[diagramCountHeader.children.length - 1]);
+    }
+
+    // Update Pipeline Diagram
+    for (let i = 0; i < diagramBody.children.length; i++) {
+        let cell = document.createElement("td");
+        diagramBody.children[i].appendChild(cell);
+    }
+
+    let offset = instructionsProcessed;
+    for (let i = dataPathState.length - 1; i >= 0; i--) {
+        if (dataPathState[i] !== null && dataPathState[i]?.instruction !== NOP) {
+            let cell = diagramBody.children[offset].lastChild as HTMLElement;
+            cell.innerText = stageLabelMap[i];
+
+            offset++;
+        }
     }
 }
